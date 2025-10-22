d4f1db404b98dd41b6aab72503025b4f7e4e2255
BigMagnus6Tips <magne.jacobsen04@gmail.com>
2025-10-22 14:32:14 +0200

    minor changes

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import whisper
 
 model = whisper.load_model("base.en")
-result = model.transcribe("recording(1).mp3")
-print(result["text"])
+
+for i in range(1, 8):
+    result = model.transcribe(f"sound_recordings/recording({i}).mp3")
+    print(f"Transcription {i}:")
+    print(result["text"])
+    print()
+
+
