259bd02d177123178c349e21fea0be39b46f66d4
Alex <alex347a@gmail.com>
2025-12-11 14:34:36 +0100

    correlation test gcc phat

--- test.py
+++ test.py
@@ -3,7 +3,5 @@ from pc_code.sound_localization.gccPhat import test_gcc_phat
 from pc_code.sound_localization.Triangulate import test123
 import numpy as np
 
-
-
-arr = [1, 2, 3, 4, 5, 6]
-print(arr[:2])
+if __name__ == "__main__":
+    test_gcc_phat()
